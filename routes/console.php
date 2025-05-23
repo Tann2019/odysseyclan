037705e52136c971ddea6b36bab0cf588f48e53e
TempUser <temp@example.com>
2025-05-23 17:42:32 -0600

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -3,8 +3,8 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('odyssey:check-live-streamers', function () {
-    $this->comment('Checking live streamers...');
-    Artisan::call('odyssey:check-live-streamers');
-    $this->info('Live streamers checked successfully.');
-})->purpose('Check live streamers and update their status')->everyFiveMinutes();
+// Artisan::command('odyssey:check-live-streamers', function () {
+//     $this->comment('Checking live streamers...');
+//     Artisan::call('odyssey:check-live-streamers');
+//     $this->info('Live streamers checked successfully.');
+// })->purpose('Check live streamers and update their status')->everyFiveMinutes();
